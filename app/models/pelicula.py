701c11d40d4d3f4575f7de4f589ab60821284de8
MisaelSanchez-SR23022-GT02 <softwaremisael@gmail.com>
2026-04-08 11:18:08 -0600

    add(models): creación del modelo para películas

---
+++ app/models/pelicula.py
@@ -0,0 +1,21 @@
+class Pelicula:
+    def __init__(self, _id_pelicula, _nombre, _duracion, _categoria):
+        self._id_pelicula = _id_pelicula
+        self._nombre = _nombre
+        self._duracion = _duracion
+        self._categoria = _categoria
+
+    def get_id(self):
+        return self._id_pelicula
+        
+    def get_nombre(self):
+        return self._nombre
+        
+    def get_duracion(self):
+        return self._duracion
+        
+    def get_categoria(self):
+        return self._categoria
+
+    def imprimir(self):    
+         return f"{self._id_pelicula:<5} | {self._nombre:<20} | {self._duracion:<5} min | {self._categoria:<10}"
