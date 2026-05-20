4d3ebac6549f9a499ddacdf77d1b3fe336b28b88
MisaelSanchez-SR23022-GT02 <softwaremisael@gmail.com>
2026-05-19 19:19:46 -0600

    refactor(funciones): implementar inyección de dependencias para películas y salas
    - Se movió la inicialización de los servicios al main.
    - Se reestructuró el módulo de funciones para recibir los IDs de películas y salas mediante inyección de dependencias.

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from app.ui.menu_principal_ui import mostrar_menu
+from app.services.pelicula_service import PeliculaService
+from app.services.sala_service import SalaService
+from app.services.funcion_service import FuncionService
+from app.ui.menu_principal_ui import mostrar_menu
 
 def main():
-    mostrar_menu()
-
+    pelicula_service = PeliculaService()
+    salas_service = SalaService()
+    funcion_service = FuncionService(pelicula_service, salas_service)
+    
+    mostrar_menu(pelicula_service, salas_service, funcion_service)
+    
 if __name__ == "__main__":
     main()
